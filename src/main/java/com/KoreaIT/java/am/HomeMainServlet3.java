9ffd508938517cfec1d890b7e69f5a0114a81ee8
horizon-river <kimpk0416@gmail.com>
2022-09-06 18:49:12 +0900

    구구단 기초, 파라미터 O

--- src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
@@ -7,17 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home/printDan")
-public class HomeMainServlet2 extends HttpServlet {
+@WebServlet("/home/main2")
+public class HomeMainServlet3 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		
-		response.getWriter().append("9단<br>");
-		
-		int dan = Integer.parseInt(request.getParameter("dan"));
-		
-		for(int i = 1; i <= 9; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan , i , dan * i));
-		}
+		response.getWriter().append("Hello World!!!!!!!!").append(request.getContextPath());
 	}
 }
