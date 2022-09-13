146a2b807364fac244de8fd85137386188fd554e
horizon-river <kimpk0416@gmail.com>
2022-09-13 18:48:29 +0900

    DB연결

---
+++ src/main/java/com/KoreaIT/java/am/ArticleListServlet.java
@@ -0,0 +1,71 @@
+package com.KoreaIT.java.am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.java.am.util.DBUtil;
+import com.KoreaIT.java.am.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		//DB 연결
+		String url = "jdbc:mysql://127.0.0.1:3306/JSPTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+		String user = "root";
+		String password = "";
+		
+		Connection conn = null;
+		
+		String driverName = "com.mysql.jdbc.Driver";
+		
+		try {
+			Class.forName(driverName);
+						
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외 : 클래스가 없습니다.");
+			System.out.println("프로그램을 종료합니다.");
+			return;
+		} 
+		
+		try {
+			conn = DriverManager.getConnection(url, "root", "");
+			
+			response.getWriter().append("Success!!");
+			DBUtil dbUtil = new DBUtil(request, response);
+			
+			SecSql sql = new SecSql();
+			
+			sql.append("SELECT * FROM article");
+			
+			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+			
+			response.getWriter().append(articleRows.toString());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
