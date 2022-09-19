8f4db5b4316964aac09362909ef3c78c192d26cb
horizon-river <kimpk0416@gmail.com>
2022-09-19 18:49:43 +0900

    회원가입 기능 구현, return false; 중복 발송 방지

---
+++ src/main/java/com/KoreaIT/java/am/servlet/MemberDoJoinServlet.java
@@ -0,0 +1,91 @@
+package com.KoreaIT.java.am.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.time.LocalDateTime;
+
+import com.KoreaIT.java.am.config.Config;
+import com.KoreaIT.java.am.util.DBUtil;
+import com.KoreaIT.java.am.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/doJoin")
+public class MemberDoJoinServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		//DB 연결
+		
+		Connection conn = null;
+		
+		String driverName = Config.getDBDriverClassName();
+		
+		try {
+			Class.forName(driverName);
+						
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외 : 클래스가 없습니다.");
+			System.out.println("프로그램을 종료합니다.");
+			return;
+		} 
+		
+		try {
+			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPassword());
+			
+			String loginId = request.getParameter("loginId");
+			
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM `member`");
+			sql.append("WHERE loginId = ?",loginId);
+			
+			int duplicateCheck = DBUtil.selectRowIntValue(conn, sql);
+			
+			if(duplicateCheck == 1) {
+				response.getWriter().append(String.format("<script>alert('중복된 아이디 입니다.'); history.back();</script>"));
+				return;
+			}
+			
+			String loginPw = request.getParameter("loginPw");
+			String name = request.getParameter("name");
+			
+			sql = SecSql.from("INSERT INTO `member`");
+			sql.append("SET regDate = NOW()");
+			sql.append(",loginId = ?",loginId);
+			sql.append(",loginPw = ?",loginPw);
+			sql.append(",`name` = ?",name);
+			
+			int id = DBUtil.insert(conn, sql);
+			
+			response.getWriter().append(String.format("<script>alert('%d번 회원님 가입 되었습니다.'); location.replace('../home/main');</script>",id));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
